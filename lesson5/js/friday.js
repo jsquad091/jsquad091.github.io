5d4f806738ab6bd66b654115a8381f325b8aa6ef
jsquad091 <jaden.garlitz@gmail.com>
2020-02-08 18:34:45 -0600

    images

--- lesson5/js/friday.js
+++ lesson5/js/friday.js
@@ -1,6 +1,6 @@
 var today = new Date();
 
-if (today.getDay() == 6 ) {
+if (today.getDay() == 5 ) {
   document.querySelector("aside").style.display = "block";
 } else {
   document.querySelector("aside").style.display = "none";
